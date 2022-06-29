cf32b9c0dbf09e51b52d501883fded626d98f419
«Aleksandr» <bourne4jokonda@gmail.com>
2022-06-29 07:19:32 +0300

    add task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -5,3 +5,13 @@
 // 2, 4 -> 16
 // !НЕ ИСПОЛЬЗОВАТЬ MATH.POW() И АНАЛОГИ!
 
+Console.WriteLine("Введите возводимое число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число-степень B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+for(int i=1; i<=B; i++)
+{
+    sum=sum*A;
+}
+Console.WriteLine($"{sum}");
